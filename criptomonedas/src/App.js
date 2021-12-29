494c9dda3e5768013d712aa285294417b24344d2
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-29 12:27:29 -0600

    Styled Components
    Se instalo styled components y se agregaron la carpeta imagenes

--- criptomonedas/src/App.js
+++ criptomonedas/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Heading = styled.h1`
+    font-family: 'lato', sans-serif;
+    color: #fff;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Heading >Hola mundo</Heading>
   );
 }
 
