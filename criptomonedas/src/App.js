f14eb7b16177108ec8b55c358b5c9702432ab801
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-29 17:28:34 -0600

    Mostrando Spinner

--- criptomonedas/src/App.js
+++ criptomonedas/src/App.js
@@ -6,6 +6,8 @@ import Formulario from './components/Formulario';
 import styled from '@emotion/styled';
 import ImagenFondo from './img/imagen-criptos.png'
 import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
+
 
 const Contenedor = styled.div`
     max-width: 900px;
@@ -49,17 +51,22 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect( () => {
     if (Object.keys(monedas).length > 0) {
 
         const cotizarCrypto = async () => {
+          setCargando(true);
+          setResultado({});
+
           const { moneda, criptomoneda } = monedas;
           const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
           const rpta = await fetch(url);
           const result = await rpta.json();
 
           setResultado(result.DISPLAY[criptomoneda][moneda]);
+          setCargando(false);
         }
 
         cotizarCrypto();
@@ -78,6 +85,8 @@ function App() {
                 <Formulario
                     setMonedas={setMonedas} />
 
+                
+                { cargando && <Spinner/> }
                 {resultado.PRICE && <Resultado resultado={resultado} /> }
             </div>
 
